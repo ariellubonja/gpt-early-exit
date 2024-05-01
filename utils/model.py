c50bf7d0560e7c7d0b14f4e9ea53aec3eb6bab4b
Ariel Lubonja <ariel@cs.jhu.edu>
2024-05-01 00:20:13 -0400

    Important: increase nr. histogram bins. rerun plots

--- utils/model.py
+++ utils/model.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 
-def plot_intermediate_model_outputs(model, keys = ['initial_hidden_states', 'post_ln1_hidden_states', 'attn_projection_output', 'post_attn_residual_hidden_states', 'post_cross_attn_hidden_states', 'post_ln2_hidden_states', 'post_feed_fwd_hidden_states', 'post_feed_fwd_residual_hidden_states']):
+def plot_intermediate_model_outputs(model, keys = ['initial_hidden_states', 'post_ln1_hidden_states', 'attn_projection_output', 'post_attn_residual_hidden_states', 'post_cross_attn_hidden_states', 'post_ln2_hidden_states', 'post_feed_fwd_hidden_states', 'post_feed_fwd_residual_hidden_states'], bins=100):
     for i in range(len(model.h)):
         int_out = model.h[i].intermediate_outputs
         for key in keys:
@@ -8,6 +8,6 @@ def plot_intermediate_model_outputs(model, keys = ['initial_hidden_states', 'pos
             im = ax1.imshow(int_out[key][0])#, aspect='auto', interpolation='nearest')
             ax1.set_title(f'Layer {i} {key} - Image')
             ax1.figure.colorbar(im, ax=ax1)
-            ax2.hist(int_out[key][0])#.ravel())#, bins=20, color='blue')
+            ax2.hist(int_out[key][0], bins)#.ravel())#, bins=20, color='blue')
             ax2.set_title(f'Layer {i} {key} - Histogram')
             plt.show()
